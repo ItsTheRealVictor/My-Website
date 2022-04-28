52c0312f4dc89ab2470b89d836f2a716f804f346
Victor Delgado <valexdelgado@gmail.com>
2022-04-28 15:16:07 -0700

    let's try to use flask

---
+++ My_Website.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route("/")
+@app.route('/home')
+def homepage():
+    return render_template('HomeIndex.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('ResumeIndex.html')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
