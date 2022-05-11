41e558a19f39eed0cc3c625cf32783a6807a5152
Victor Delgado <valexdelgado@gmail.com>
2022-05-11 16:09:31 -0700

    cleaning stuff up

--- My_Website.py
+++ My_Website.py
@@ -1,15 +1,19 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')
+app = Flask(__name__)
 
-@app.route("/")
-@app.route('/home')
+@app.route("/home")
 def homepage():
     return render_template('HomeIndex.html')
+
 
 @app.route('/resume')
 def resume():
     return render_template('ResumeIndex.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('ContactIndex.html')
 
 
 if __name__ == "__main__":
