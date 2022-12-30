9e725124054e94b04f811673d6f931bfbaf9e760
Victor Delgado <valexdelgado@gmail.com>
2022-12-30 11:45:33 -0800

    trying to correct a key error

--- models.py
+++ models.py
@@ -23,7 +23,7 @@ class User(db.Model):
     __tablename__ = 'users'
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    username = db.Column(db.Text, nullable=False, unique=True)
+    username = db.Column(db.Text, nullable=False)
     password = db.Column(db.Text, nullable=False)
     
     @classmethod
